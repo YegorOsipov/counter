73cb1650402b270e139b0012f8694de7d766aae7
Yegor <gogsyan11@gmail.com>
2022-02-13 20:05:58 +0300

    сделал disabled кнопок

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styles from './App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCounter] = useState(0);
+    const changeCounter = () => {
+        setCounter(counter + 1);
+    }
+    return (
+        <div>
+            <div className={styles.main}>
+                <div className={styles.counter}>{counter}</div>
+                <div className={styles.buttonsWrapper}>
+                    {counter === 5 ?
+                        <button className={styles.btnDisabled} disabled>inc</button> :
+                        <button className={styles.btn} onClick={changeCounter}>inc</button>
+                    }
+
+                    {counter !== 5 ?
+                        <button className={styles.btnDisabled}>reset</button> :
+                        <button className={styles.btn}>reset</button>
+                    }
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
